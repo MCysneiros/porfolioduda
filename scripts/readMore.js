ecacbbd8f2faedb0843e3d903baebfa6129957d8
Mcysneiros <msocys@gmail.com>
2022-07-04 21:57:14 -0300

    refactor:some changes

---
+++ scripts/readMore.js
@@ -0,0 +1,25 @@
+let noOfCharac = 150;
+
+let contents = document.querySelectorAll('.content');
+
+contents.forEach(content => {
+	if (content.textContent.length < noOfCharac) {
+		content.nextElementSibling.style.display = 'none';
+	} else {
+		let displayText = content.textContent.slice(0, noOfCharac);
+
+		let moreText = content.textContent.slice(noOfCharac);
+
+		content.innerHTML = `${displayText}<span class="dots">...</span><span class="hide more">${moreText}</span>`;
+	}
+});
+
+function readMore(btn) {
+	let post = btn.parentElement;
+	post.querySelector('.dots').classList.toggle('hide');
+	post.querySelector('.more').classList.toggle('hide');
+
+	btn.textContent === 'Leia mais'
+		? (btn.textContent = 'Leia menos')
+		: (btn.textContent = 'Leia mais');
+}
